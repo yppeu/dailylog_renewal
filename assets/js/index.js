aeab9413bcf13aae80525bf88da4581bae35a149
sua yeon <yppeu@naver.com>
2022-12-09 22:09:06 +0900

    1209

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// var $circle = $(".circle");
+
+// function moveCircle(e) {
+//   TweenLite.to($circle, 0.3, {
+//     css: {
+//       left: e.pageX,
+//       top: e.pageY,
+//     },
+//   });
+// }
+
+// $(window).on("mousemove", moveCircle);
